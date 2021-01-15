22e6b83a9a0c7186f2014b226045303075fb0a6f
Leandro <lefinochio@hotmail.com>
2021-01-15 11:39:54 -0300

    Alteração frases aviso treinamento Staff

--- src/components/ListasAvisos/Staff/index.js
+++ src/components/ListasAvisos/Staff/index.js
@@ -8,22 +8,21 @@ export default () => {
       <h1>Treinamento Staff</h1>
       <ul>
         <li>
-          A equipe do Staff da CGSBC está recrutando e para isso se faz
-          necessário um treinamento totalmente presencial respeitando as medidas
-          de segurança para capacitação dos interessados em contribuir no
-          ministério.
+          A equipe do Staff da CGSBC está recrutando, e para isso é necessário
+          um treinamento presencial, respeitando as medidas de segurança, para
+          capacitação dos interessados em contribuir no ministério.
         </li>
         <li>
           O treinamento também será oferecido para aqueles que já atuam no
-          ministério visando crescimento, aperfeiçoamento e um melhor desempenho
-          em nossas transmissões.
+          ministério, visando crescimento, aperfeiçoamento e um melhor
+          desempenho em nossas transmissões.
         </li>
         <li>O treinamento será gratuito no dia 06/02 das 08h00 às 13h00</li>
         <li>
           Qualquer dúvida procurar por Diego Aoyagi, Jefferson Vieira, Leonardo
           Veloso, Luma Salazar ou Vinicius Sena
         </li>
-        <li>As inscrições poderão ser feitas somente até o dia 03/02</li>
+        <li>Inscrições até o dia 03/02</li>
         <li>* Obrigatório</li>
       </ul>
     </Container>
