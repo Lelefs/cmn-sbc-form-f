aba4117a41977e5f9bb673c570cec535f610a596
Leandro Finochio <lefinochio@hotmail.com>
2021-02-15 20:30:45 -0300

    Colocar última LI

--- src/components/ListasAvisos/Cultos/index.js
+++ src/components/ListasAvisos/Cultos/index.js
@@ -8,6 +8,7 @@ export default () => {
       <h1>Culto presencial Comunidade da Graça SBC</h1>
       <ul>
         <li>Não é mais necessário fazer inscrição para o culto presencial.</li>
+        <li />
       </ul>
     </Container>
   );
