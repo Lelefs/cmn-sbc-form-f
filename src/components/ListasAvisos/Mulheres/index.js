bba344366271f0cf85b90cbdb470afeb50fb78f5
Jeff <jefcarmo01@hotmail.com>
2021-02-26 17:41:44 -0300

    Correção no form das mulheres intercessoras

--- src/components/ListasAvisos/Mulheres/index.js
+++ src/components/ListasAvisos/Mulheres/index.js
@@ -10,23 +10,15 @@ export default () => {
         <li>
             Nossos encontro presenciais estão de volta ! <br />
             Seguindo todas as normas de segurança da OMS voltaremos com nossas reuniões precisais.
-            A reunião ocorrerá no dia 13/03 às 15:00 em nossa igreja. Esperemos por vocês ❤
+            A reunião ocorrerá no dia 13/03 às 15:00 em nossa igreja. Esperamos por vocês ❤
         </li>
         <li>
           Em caso de dúvidas, entre em contato pelo nosso WhatsApp
           (11)93262-1680.
         </li>
         <li>
-          Os cultos serão realizados de portas fechadas e as portas fecharão
-          pontualmente às 15h30, respectivamente.
-        </li>
-        <li>
           Venha de máscara e siga as orientações das diáconisas e cooperadoras
           para distanciamento e higienização.
-        </li>
-        <li>
-          Recomendamos que idosos, crianças e pessoas que fazem parte do grupo
-          de risco de Covid-19 não participem das reuniões presenciais.
         </li>
         <li>*Obrigatório</li>
       </ul>
