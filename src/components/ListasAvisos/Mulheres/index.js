2df804e8d80591ba427eaee8de22441720d16441
Leandro Finochio <lefinochio@hotmail.com>
2021-03-03 15:23:08 -0300

    Correção lista aviso culto mulheres

--- src/components/ListasAvisos/Mulheres/index.js
+++ src/components/ListasAvisos/Mulheres/index.js
@@ -8,16 +8,10 @@ export default () => {
       <h1>Mulheres Intercessoras - Comunidade da Graça SBC</h1>
       <ul>
         <li>
-<<<<<<< HEAD
           Nossos encontros presenciais estão de volta ! <br />
           Seguindo todas as normas de segurança da OMS voltaremos com nossas
           reuniões presenciais. A reunião ocorrerá no dia 13/03 às 15h00 em
           nossa igreja. Esperamos por vocês ❤
-=======
-            Nossos encontro presenciais estão de volta ! <br />
-            Seguindo todas as normas de segurança da OMS voltaremos com nossas reuniões precisais.
-            A reunião ocorrerá no dia 13/03 às 15:00 em nossa igreja. Esperamos por vocês ❤
->>>>>>> master
         </li>
         <li>
           Em caso de dúvidas, entre em contato pelo nosso WhatsApp
